28befd7d442325b7e52298e2209a1c9bd3e7fcbd
quanzhong.wang <quanzhong.wang@25020853-03>
2026-07-06 13:16:04 +0800

    格式化代码

--- src/views/user/modifyPwd.vue
+++ src/views/user/modifyPwd.vue
@@ -1,31 +1,21 @@
 <template>
   <view class="container">
- 
     <view class="form-wrapper">
-    
       <view class="input-item phone-box">
-     
         <text class="phone-text">188 **** 8888</text>
       </view>
 
- 
-          <VerifyCodeInput 
-             v-model="formData.code" 
-             phone="188888888"
-            
-           />
+      <VerifyCodeInput v-model="formData.code" phone="188888888" />
 
- 
       <view class="input-item">
-      
         <input
           class="input"
           :password="!showPassword1"
-					maxlength="12"
+          maxlength="12"
           placeholder="请输入新密码"
           v-model="formData.password1"
         />
-  
+
         <uni-icons
           :type="showPassword1 ? 'eye-slash' : 'eye'"
           size="20"
@@ -34,12 +24,11 @@
         />
       </view>
 
- 
       <view class="input-item">
         <uni-icons type="locked" size="20" color="#999" />
         <input
           class="input"
-					maxlength="12"
+          maxlength="12"
           :password="!showPassword2"
           placeholder="请再次输入密码"
           v-model="formData.password2"
@@ -53,7 +42,6 @@
       </view>
     </view>
 
-  
     <view class="btn-area">
       <button class="submit-btn" @click="handleSubmit">确定</button>
     </view>
@@ -61,14 +49,14 @@
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
-import VerifyCodeInput from '@/components/verify-code/verify-code.vue';
-import {ChangePhone} from "@/axios/mine.js"
+import { ref, reactive } from "vue";
+import VerifyCodeInput from "@/components/verify-code/verify-code.vue";
+import { ChangePhone } from "@/axios/mine.js";
 // --- 数据定义 ---
 const formData = reactive({
-  code: '',
-  password1: '',
-  password2: '',
+  code: "",
+  password1: "",
+  password2: "",
 });
 
 const showPassword1 = ref(false);
@@ -76,23 +64,25 @@ const showPassword2 = ref(false);
 
 // 提交表单
 const handleSubmit = () => {
-  if (!formData.code) return uni.showToast({ title: '请输入验证码', icon: 'none' });
-  if (!formData.password1) return uni.showToast({ title: '请输入新密码', icon: 'none' });
+  if (!formData.code)
+    return uni.showToast({ title: "请输入验证码", icon: "none" });
+  if (!formData.password1)
+    return uni.showToast({ title: "请输入新密码", icon: "none" });
   if (formData.password1 !== formData.password2)
-    return uni.showToast({ title: '两次密码不一致', icon: 'none' });
-console.log(formData)
-ChangePhone({...formData}).then(res => {
-	// 这里执行提交逻辑
-	uni.showToast({ title: '密码重置成功', icon: 'success' });
-}).catch()
-
+    return uni.showToast({ title: "两次密码不一致", icon: "none" });
+  console.log(formData);
+  ChangePhone({ ...formData })
+    .then((res) => {
+      // 这里执行提交逻辑
+      uni.showToast({ title: "密码重置成功", icon: "success" });
+    })
+    .catch();
 };
 </script>
 
 <style lang="scss" scoped>
-
 .container {
-  padding:  60rpx;
+  padding: 60rpx;
   background-color: #fff;
   min-height: 100vh;
 }
@@ -114,7 +104,6 @@ ChangePhone({...formData}).then(res => {
   }
 }
 
-
 .input {
   flex: 1;
   height: 100%;
@@ -135,7 +124,7 @@ ChangePhone({...formData}).then(res => {
   font-family: PingFangSC, PingFang SC;
   font-weight: 400;
   font-size: 28rpx;
-  color: #FF8500;
+  color: #ff8500;
   position: absolute;
   right: 20rpx;
   padding: 10rpx 20rpx;
@@ -159,7 +148,7 @@ ChangePhone({...formData}).then(res => {
   display: flex;
   align-items: center;
   justify-content: center;
-  
+
   /* 去除按钮默认样式 */
   &::after {
     border: none;
